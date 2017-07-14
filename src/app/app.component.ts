11a17b5a277d9246ce440dba1b54836cd914ae8e
francop <franco.poveda@gmail.com>
2017-07-14 04:43:31 -0300

    chaning DO values now works

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,33 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { MQTTService } from 'app/services/mqtt';
+import { MdSlideToggleModule } from '@angular/material';
+
+
 
 
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 
 })
 export class AppComponent {
-      public publish: Function;
-      public currentO1: number[];
-      public currentO2: number[];
+    public publish: Function;
+    public currentO1: number[];
+    public currentO2: number[];
 
-      constructor(public _mqService: MQTTService) {
-          this.publish = _mqService.publish;
- }
- public setDigitalOutput() {
+    public a1: MdSlideToggleModule;
+    public a2: MdSlideToggleModule;
+    public a3: MdSlideToggleModule;
+    public a4: MdSlideToggleModule;
 
-    console.dir(this.currentO1);
-    const sum = this.currentO1.reduce(function(valorAnterior, valorActual, indice, vector){
-  return Number(valorAnterior) + Number(valorActual);
-});
-console.log(sum);
-    console.log(Number(sum).toString(2));
+    public b1: MdSlideToggleModule;
+    public b2: MdSlideToggleModule;
+    public b3: MdSlideToggleModule;
+    public b4: MdSlideToggleModule;
 
 
+    constructor(public _mqService: MQTTService) {
+        this.publish = _mqService.publish;
+    }
+    public setDigitalOutput(a1, a2, a3, a4, b1, b2, b3, b4) {
 
- }
+        console.dir(a1, a2, a3, a4, b1, b2, b3, b4);
+        let do1 = [a1, a2, a3, a4].map(x => {
+            return (x == false || x == undefined) ? 1 : 0;
+        });
+        let do2 = [b1, b2, b3, b4].map(x => {
+            return (x == false || x == undefined) ? 1 : 0;
+        });
+        const updateDO = {
+            "do1": do1,
+            "do2": do2
+        }
+        this._mqService.publish(JSON.stringify(updateDO));
 
- }
+
+
+    }
+
+}
